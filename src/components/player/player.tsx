4dc89b7fd6c761c977d24a8d329e072501bf04fb
Júlio César <jc10ferreira@gmail.com>
2024-05-05 07:21:09 -0300

    feat: :sparkles: Introduces timeout handling for the user

--- src/components/player/player.tsx
+++ src/components/player/player.tsx
@@ -7,72 +7,109 @@ import { PlayerPlayPauseBtn } from './player-play-pause-btn';
 import { PlayerRadioInfo } from './player-radio-info';
 import { PlayerVolume } from './player-volume';
 
-export function Player() {
-  const {
-    currentRadio,
-    volume,
-    selectRadio,
-    audioRef,
-    handleIsFetching,
-    playPause,
-  } = useRadio();
+const WAITING_TIMEOUT = 3000 * 10; // 30s = 3000 * 10
 
-  useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.volume = volume[0] / 100;
-    }
-  }, [volume]);
+export function Player() {
+  let audioWaitingTimeout: NodeJS.Timeout;
+
+  const { volume, currentRadio, selectRadio, audioRef, handleIsFetching } =
+    useRadio();
 
   useEffect(() => {
     if (audioRef.current && currentRadio) {
       handleIsFetching(true);
-
-      if (!audioRef.current.paused) {
-        audioRef.current.pause();
-      }
+      clearTimeout(audioWaitingTimeout);
 
       audioRef.current.src = currentRadio.url_resolved || currentRadio.url;
+      audioRef.current.volume = volume[0] / 100;
 
-      audioRef.current.play().finally(() => {
-        handleIsFetching(false);
-      });
+      audioRef.current.addEventListener('loadeddata', handleAudioLoadedData);
+      audioRef.current.addEventListener('waiting', handleAudioWaiting);
+      audioRef.current.addEventListener('canplay', handleAudioCanPlay);
+
+      audioRef.current
+        .play()
+        .then(() => {
+          handleIsFetching(false);
+          clearTimeout(audioWaitingTimeout);
+        })
+        .finally(() => {
+          handleIsFetching(false);
+        });
+
+      return () => {
+        audioRef.current.removeEventListener(
+          'loadeddata',
+          handleAudioLoadedData
+        );
+        audioRef.current.removeEventListener('waiting', handleAudioWaiting);
+        audioRef.current.removeEventListener('canplay', handleAudioCanPlay);
+      };
     }
   }, [currentRadio]);
 
   const handleAudioLoadedData = () => {
+    clearTimeout(audioWaitingTimeout);
     handleIsFetching(false);
   };
 
   const handleAudioWaiting = () => {
     handleIsFetching(true);
+
+    audioWaitingTimeout = setTimeout(() => {
+      handleIsFetching(false);
+
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+
+      selectRadio(null);
+
+      toast.error('Long duration elapsed without audio.', {
+        description: 'Failed to connect to the radio station. Please retry.',
+        dismissible: false,
+        action: {
+          label: 'Retry',
+          onClick: handleRetry,
+        },
+      });
+    }, WAITING_TIMEOUT);
   };
 
   const handleAudioCanPlay = () => {
+    clearTimeout(audioWaitingTimeout);
     handleIsFetching(false);
   };
 
-  const handleAudioError = () => {
-    toast.error('Failed to load radio stream.', {
-      description:
-        'The radio stream could not be loaded. Please try again later.',
-    });
-    handleIsFetching(false);
-    selectRadio(null);
-    playPause();
+  const handleRetry = async () => {
+    clearTimeout(audioWaitingTimeout);
+
+    toast.promise(
+      (async () => {
+        try {
+          handleIsFetching(true);
+          await selectRadio(currentRadio);
+          handleIsFetching(false);
+          toast.dismiss();
+        } catch (error) {
+          handleIsFetching(false);
+          toast.error(`Failed to connect to ${currentRadio.name}.`, {
+            description: error.message,
+          });
+          selectRadio(null);
+        }
+      })(),
+      {
+        loading: `Retrying to connect to ${currentRadio.name}...`,
+        success: `Connected to ${currentRadio.name}.`,
+        error: `Failed to connect to ${currentRadio.name}.`,
+      }
+    );
   };
 
   return (
     <div className='fill-available w-full h-40 fixed bottom-0 content-center sm:h-20 bg-sidebar p-4'>
-      <audio
-        ref={audioRef}
-        src={currentRadio?.url || currentRadio?.url_resolved}
-        onLoadedData={handleAudioLoadedData}
-        onWaiting={handleAudioWaiting}
-        onCanPlay={handleAudioCanPlay}
-        onError={handleAudioError}
-        controls
-        hidden
-      />
+      <audio ref={audioRef} controls hidden />
       <div className='flex flex-col sm:flex-row items-center justify-between gap-4'>
         <div className='flex gap-2 items-center justify-center sm:justify-start w-full'>
           <PlayerPlayPauseBtn />
