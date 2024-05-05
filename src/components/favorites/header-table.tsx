a5c1faa78ed43928cc7e82273d24ad7513a6392a
Júlio César <jc10ferreira@gmail.com>
2024-05-05 03:27:33 -0300

    feat: :sparkles: Adds an alert for unwary users

--- src/components/favorites/header-table.tsx
+++ src/components/favorites/header-table.tsx
@@ -1,3 +1,4 @@
+import { Alert, AlertDescription, AlertTitle } from '@components/ui/alert';
 import { Button } from '@components/ui/button';
 import {
   DropdownMenu,
@@ -14,7 +15,7 @@ import {
   SelectValue,
 } from '@components/ui/select';
 import { Table } from '@tanstack/react-table';
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown, Info } from 'lucide-react';
 import { SetStateAction } from 'react';
 import { FilterSelectType } from './data-table';
 
@@ -30,58 +31,75 @@ export function HeaderTable<TData>({
   table,
 }: HeaderTableProps<TData>) {
   return (
-    <div className='flex flex-wrap mb-2 md:mb-0 items-center md:justify-between gap-2'>
-      <div className='flex flex-wrap md:flex-nowrap items-center py-0 md:py-4 gap-2 w-full md:w-1/2'>
-        <Input
-          placeholder={`Filter by ${
-            filterBy === 'name' ? 'station' : filterBy
-          }...`}
-          value={(table.getColumn(filterBy)?.getFilterValue() as string) ?? ''}
-          onChange={(event) => {
-            const filterValue = event.target.value;
-            table.getColumn(filterBy)?.setFilterValue(filterValue);
-          }}
-        />
+    <>
+      <div className='flex flex-wrap mb-2 md:mb-0 items-center md:justify-between gap-2'>
+        <div className='flex flex-wrap md:flex-nowrap items-center py-0 md:py-4 gap-2 w-full md:w-1/2'>
+          <Input
+            placeholder={`Filter by ${
+              filterBy === 'name' ? 'station' : filterBy
+            }...`}
+            value={
+              (table.getColumn(filterBy)?.getFilterValue() as string) ?? ''
+            }
+            onChange={(event) => {
+              const filterValue = event.target.value;
+              table.getColumn(filterBy)?.setFilterValue(filterValue);
+            }}
+          />
 
-        <Select
-          value={filterBy}
-          onValueChange={(value) => setFilterBy(value as FilterSelectType)}
-        >
-          <SelectTrigger className='w-full md:w-[180px]' aria-label='Filter by'>
-            <SelectValue placeholder='Filter by' />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value='name'>Station</SelectItem>
-            <SelectItem value='country'>Country</SelectItem>
-            <SelectItem value='tags'>Tags</SelectItem>
-          </SelectContent>
-        </Select>
+          <Select
+            value={filterBy}
+            onValueChange={(value) => setFilterBy(value as FilterSelectType)}
+          >
+            <SelectTrigger
+              className='w-full md:w-[180px]'
+              aria-label='Filter by'
+            >
+              <SelectValue placeholder='Filter by' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='name'>Station</SelectItem>
+              <SelectItem value='country'>Country</SelectItem>
+              <SelectItem value='tags'>Tags</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='outline' className='w-full md:w-auto md:ml-auto'>
+              Columns <ChevronDown size={16} className='ml-2' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className='capitalize'
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
 
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant='outline' className='w-full md:w-auto md:ml-auto'>
-            Columns <ChevronDown size={16} className='ml-2' />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align='end'>
-          {table
-            .getAllColumns()
-            .filter((column) => column.getCanHide())
-            .map((column) => {
-              return (
-                <DropdownMenuCheckboxItem
-                  key={column.id}
-                  className='capitalize'
-                  checked={column.getIsVisible()}
-                  onCheckedChange={(value) => column.toggleVisibility(!!value)}
-                >
-                  {column.id}
-                </DropdownMenuCheckboxItem>
-              );
-            })}
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+      <Alert className='mb-4'>
+        <Info className='h-4 w-4' />
+        <AlertTitle>Attention!</AlertTitle>
+        <AlertDescription>
+          To search for non-favorite radio stations, please use the sidebar.
+        </AlertDescription>
+      </Alert>
+    </>
   );
 }
--- src/components/favorites/responsive-header-table.tsx
+++ src/components/favorites/responsive-header-table.tsx
@@ -1,5 +1,6 @@
 import { Input } from '@components/ui/input';
 
+import { Alert, AlertDescription, AlertTitle } from '@components/ui/alert';
 import {
   Select,
   SelectContent,
@@ -7,6 +8,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@components/ui/select';
+import { Info } from 'lucide-react';
 import { SetStateAction } from 'react';
 import { FilterSelectType } from './data-table';
 
@@ -24,30 +26,43 @@ export function ResponsiveHeaderTable({
   setFilterValue,
 }: ResponsiveHeaderTableProps) {
   return (
-    <div className='flex flex-wrap md:mb-0 items-center md:justify-between gap-2'>
-      <div className='flex flex-wrap md:flex-nowrap items-center py-0 md:py-4 gap-2 w-full md:w-1/2'>
-        <Input
-          placeholder={`Filter by ${
-            filterBy === 'name' ? 'station' : filterBy
-          }...`}
-          value={filterValue}
-          onChange={(event) => setFilterValue(event.target.value)}
-        />
+    <>
+      <div className='flex flex-wrap md:mb-0 items-center md:justify-between gap-2'>
+        <div className='flex flex-wrap md:flex-nowrap items-center py-0 md:py-4 gap-2 w-full md:w-1/2'>
+          <Input
+            placeholder={`Filter by ${
+              filterBy === 'name' ? 'station' : filterBy
+            }...`}
+            value={filterValue}
+            onChange={(event) => setFilterValue(event.target.value)}
+          />
 
-        <Select
-          value={filterBy}
-          onValueChange={(value) => setFilterBy(value as FilterSelectType)}
-        >
-          <SelectTrigger className='w-full md:w-[180px]' aria-label='Filter by'>
-            <SelectValue placeholder='Filter by' />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value='name'>Station</SelectItem>
-            <SelectItem value='country'>Country</SelectItem>
-            <SelectItem value='tags'>Tags</SelectItem>
-          </SelectContent>
-        </Select>
+          <Select
+            value={filterBy}
+            onValueChange={(value) => setFilterBy(value as FilterSelectType)}
+          >
+            <SelectTrigger
+              className='w-full md:w-[180px]'
+              aria-label='Filter by'
+            >
+              <SelectValue placeholder='Filter by' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='name'>Station</SelectItem>
+              <SelectItem value='country'>Country</SelectItem>
+              <SelectItem value='tags'>Tags</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
-    </div>
+
+      <Alert className='my-2'>
+        <Info className='h-4 w-4' />
+        <AlertTitle>Attention!</AlertTitle>
+        <AlertDescription>
+          To search for non-favorite radio stations, please use the sidebar.
+        </AlertDescription>
+      </Alert>
+    </>
   );
 }
