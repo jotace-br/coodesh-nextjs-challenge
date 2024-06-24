215a2356d61fd6b218f586381b9bc4e344aab450
Júlio César <jc10ferreira@gmail.com>
2024-06-23 22:58:14 -0300

    feat(player-volume): :sparkles: Handle the last user's volume and restore to it after unmute the player

--- src/components/player/player-volume.tsx
+++ src/components/player/player-volume.tsx
@@ -6,22 +6,37 @@ import {
   TooltipTrigger,
 } from '@components/ui/tooltip';
 import { useRadio } from 'contexts/radio-context';
+import { useDebounce } from 'hooks/use-debounce';
 import { Volume1, Volume2, VolumeX } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 export function PlayerVolume() {
   const { volume, adjustVolume, audioRef } = useRadio();
+  const [lastVolume, setLastVolume] = useState(volume[0] || 100);
+  const debouncedVolume = useDebounce(volume[0], 300);
+
+  useEffect(() => {
+    if (debouncedVolume > 0) {
+      setLastVolume(debouncedVolume);
+    }
+  }, [debouncedVolume]);
 
   const handleMute = () => {
-    const newVolume = volume[0] === 0 ? 100 : 0;
+    const newVolume = volume[0] === 0 ? lastVolume : 0;
     handleVolumeChange([newVolume]);
   };
 
   const handleVolumeChange = (value: number[]) => {
+    const audio = audioRef.current;
     adjustVolume(value);
 
-    if (audioRef.current) {
-      audioRef.current.volume = value[0] / 100;
+    if (audio) {
+      audio.volume = value[0] / 100;
     }
+  };
+
+  const handleVolumeCommit = (value: number[]) => {
+    setLastVolume(value[0]);
   };
 
   return (
@@ -30,7 +45,9 @@ export function PlayerVolume() {
         <Tooltip>
           <TooltipTrigger asChild>
             <button onClick={handleMute}>
-              <span className='sr-only'>Mute/Unmute</span>
+              <span className='sr-only'>
+                {volume[0] === 0 ? 'Unmute' : 'Mute'}
+              </span>
               {volume[0] === 0 ? (
                 <VolumeX />
               ) : volume[0] < 50 ? (
@@ -52,6 +69,7 @@ export function PlayerVolume() {
         defaultValue={[70]}
         value={volume}
         onValueChange={handleVolumeChange}
+        onValueCommit={handleVolumeCommit}
       />
     </div>
   );
